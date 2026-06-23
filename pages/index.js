40b93743b65387d6722742d78fb0e7c004462e14
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2026-06-23 01:13:00 +0000

    Configura Prettier - Adiciona `lint:fix` e `lint:check`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default Home;
